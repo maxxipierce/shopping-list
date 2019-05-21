5aa78582916edb18a171304926e7db7c49ff8515
Kenzie Pierce <maxxipierce@gmail.com>
2019-05-21 00:03:14 -0600

    made shopping cart

---
+++ index.js
@@ -0,0 +1,26 @@
+$('#js-shopping-list-form').submit(function (event) {
+  event.preventDefault();
+  $('.shopping-list').append(`<li>
+    <span class="shopping-item">${$('#shopping-list-entry').val()}</span>
+    <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`);
+  $('#shopping-list-entry').val("");
+});
+
+$('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+  event.preventDefault();
+  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+$('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+  event.preventDefault();
+  $(this).closest('li').remove();
+});
+
